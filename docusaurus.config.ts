c9d27bfbbd27d1bde7d657764275b3459e8d8d46
Adrian Busse <git@adrianmxb.com>
2024-02-05 20:37:50 +0000

    chore: serve docs on base path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -36,6 +36,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
+          routeBasePath: "/",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/highcard-dev/docs/edit/master",
