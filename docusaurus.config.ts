55d880d0ea35a21dcc3d83c2679bd6713846e6a3
Adrian Busse <git@adrianmxb.com>
2024-02-05 20:39:24 +0000

    chore: serve docs on base path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -36,6 +36,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
+          path: "docs",
           routeBasePath: "/",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
